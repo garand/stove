e2fad95a558fabbb51ffe17d503f16804e6fdffe
Anthony Garand <anthonygarand@gmail.com>
2014-11-09 18:37:38 -0500

    Add radio button selector for wood level

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ if ( $_POST ) {
 
   $outside_temp = mysql_real_escape_string( $outside_temp );
   $stove_temp = mysql_real_escape_string( $stove_temp );
-  $percent_full = mysql_real_escape_string( $percent_full );
-  $percent_filled_to = mysql_real_escape_string( $percent_filled_to );
+  $pre_fill_level = mysql_real_escape_string( $pre_fill_level );
+  $post_fill_level = mysql_real_escape_string( $post_fill_level );
   $filled_by = mysql_real_escape_string( $filled_by );
 
-  $sql = "INSERT INTO log (outside_temp,stove_temp,percent_full,percent_filled_to,filled_by,datetime) VALUES ('$outside_temp','$stove_temp','$percent_full','$percent_filled_to','$filled_by', NOW())";
+  $sql = "INSERT INTO log (outside_temp,stove_temp,pre_fill_level,post_fill_level,filled_by,datetime) VALUES ('$outside_temp','$stove_temp','$pre_fill_level','$post_fill_level','$filled_by', NOW())";
   $data = mysql_query($sql,$link);
 
   setcookie( "filled_by" , $filled_by, time() + (10 * 365 * 24 * 60 * 60) );
@@ -24,11 +24,13 @@ if ( $_POST ) {
   ';
 }
 else {
-  $default_outside_temp = file_get_contents( 'http://api.openweathermap.org/data/2.5/weather?lat=43.037254&lon=-82.503141' );
-  $default_outside_temp = json_decode($default_outside_temp, true);
-  $default_outside_temp = round((( $default_outside_temp["main"]["temp"] - 273.15 ) * 1.8) + 32);
   $default_filled_by = $_COOKIE["filled_by"];
 }
+
+$default_outside_temp = file_get_contents( 'http://api.openweathermap.org/data/2.5/weather?lat=43.037254&lon=-82.503141' );
+$default_outside_temp = json_decode($default_outside_temp, true);
+$default_outside_temp = round((( $default_outside_temp["main"]["temp"] - 273.15 ) * 1.8) + 32);
+
 
 ?>
 <?php include 'header.php' ?>
@@ -38,10 +40,31 @@ else {
     <input type="hidden" name="outside_temp" id="outside_temp" value="<?php echo $default_outside_temp ?>">
     <label for="stove_temp">Stove Temperature</label>
     <input type="tel" name="stove_temp" id="stove_temp">
-    <label for="percent_full">Pre-Fill Wood Level</label>
-    <input type="tel" name="percent_full" id="percent_full">
-    <label for="percent_filled_to">Post-Fill Wood Level</label>
-    <input type="tel" name="percent_filled_to" id="percent_filled_to">
+
+    <label for="pre_fill_level">Pre-Fill Wood Level</label>
+    <input type="radio" name="pre_fill_level" id="pre_fill_level_0" value="0">
+    <label for="pre_fill_level_0">Empty</label>
+    <input type="radio" name="pre_fill_level" id="pre_fill_level_25" value="25">
+    <label for="pre_fill_level_25">1/4</label>
+    <input type="radio" name="pre_fill_level" id="pre_fill_level_50" value="50">
+    <label for="pre_fill_level_50">1/2</label>
+    <input type="radio" name="pre_fill_level" id="pre_fill_level_75" value="75">
+    <label for="pre_fill_level_75">3/4</label>
+    <input type="radio" name="pre_fill_level" id="pre_fill_level_100" value="100">
+    <label for="pre_fill_level_100">Full</label>
+
+    <label for="post_fill_level">Post-Fill Wood Level</label>
+    <input type="radio" name="post_fill_level" id="post_fill_level_0" value="0">
+    <label for="post_fill_level_0">Empty</label>
+    <input type="radio" name="post_fill_level" id="post_fill_level_25" value="25">
+    <label for="post_fill_level_25">1/4</label>
+    <input type="radio" name="post_fill_level" id="post_fill_level_50" value="50">
+    <label for="post_fill_level_50">1/2</label>
+    <input type="radio" name="post_fill_level" id="post_fill_level_75" value="75">
+    <label for="post_fill_level_75">3/4</label>
+    <input type="radio" name="post_fill_level" id="post_fill_level_100" value="100">
+    <label for="post_fill_level_100">Full</label>
+
     <label for="filled_by">Filled By</label>
     <input type="text" name="filled_by" id="filled_by" value="<?php echo $default_filled_by ?>">
     <input type="submit" value="Submit Log">
