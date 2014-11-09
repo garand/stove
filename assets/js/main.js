c8a5735a060a04faf8da2bc252a766d06b8da259
Anthony Garand <anthonygarand@gmail.com>
2014-11-09 15:28:07 -0500

    Remove date/time fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
 $(function(){
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
-  var hour   = today.getHours();
-  var minute = today.getMinutes();
-  var second = today.getSeconds();
-  if (dd<10){dd='0'+dd}
-  if (mm<10){mm='0'+mm}
-  date = yyyy+'-'+mm+'-'+dd;
-  time = hour+':'+minute;
-  am_pm = "AM"
-  if (hour>=12){am_pm=" PM"}
 
-  $( "#fill_date" ).val( date );
-  $( "#fill_time" ).val( time + am_pm );
 });
--- index.php
+++ index.php
@@ -1,24 +1,15 @@
-<?php
-
-$default_filled_by = $_COOKIE["filled_by"];
-
-?>
+<?php $default_filled_by = $_COOKIE["filled_by"]; ?>
 <?php include 'header.php' ?>
 <div class="content">
   <form action="/log-entry.php" method="post">
-    <label for="fill_date">Date</label>
-    <input type="fill_date" name="fill_date" id="fill_date">
-    <label for="fill_time">Time</label>
-    <input type="fill_time" name="fill_time" id="fill_time">
-    <label for="temp">Temperature</label>
-    <input type="temp" name="temp" id="temp">
+    <label for="stove_temp">Stove Temperature</label>
+    <input type="stove_temp" name="stove_temp" id="stove_temp">
     <label for="percent_full">% Full (when you arrived)</label>
     <input type="text" name="percent_full" id="percent_full">
     <label for="percent_filled_to">% Filled To</label>
     <input type="text" name="percent_filled_to" id="percent_filled_to">
     <label for="filled_by">Filled By</label>
     <input type="text" name="filled_by" id="filled_by" value="<?php echo $default_filled_by ?>">
-
     <input type="submit" value="Submit">
   </form>
 </div>
