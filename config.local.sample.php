563d087fa81deb18a37315785b173c7f3f63499c
Anthony Garand <anthonygarand@gmail.com>
2014-11-09 22:38:15 -0500

    Add sample local config

---
+++ config.local.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+$db_name = ""; // Database Name
+$db_user = ""; // Database User
+$db_pass = ""; // Database Password
+$db_host = ""; // Database Host
+
+putenv("twilio_sid="); // Twilio SID
+putenv("twilio_token="); // Twilio Token
+putenv("twilio_to="); // Twilio To
+putenv("twilio_from="); // Twilio From
