536f1332418685ff43d30a0f958b633ce3af9dfe
Anthony Garand <anthonygarand@gmail.com>
2014-11-11 13:59:23 -0500

    Add outside temperature enviorment variable placeholders to sample config

--- config.local.sample.php
+++ config.local.sample.php
@@ -5,6 +5,9 @@ $db_user = ""; // Database User
 $db_pass = ""; // Database Password
 $db_host = ""; // Database Host
 
+putenv("outside_temp_lat="); // Latitude location of stove
+putenv("outside_temp_long="); // Longitude location of stove
+
 putenv("twilio_sid="); // Twilio SID
 putenv("twilio_token="); // Twilio Token
 putenv("twilio_to="); // Twilio To
